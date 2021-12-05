cc7fb9a5d4f6a46d6a626fa92a0459bfd87f39e2
Mario Huizar <mario.huizar@gmail.com>
2021-12-05 20:20:12 +0100

    Day 5 Part 2

--- advent2021/src/advent2021/Day05.scala
+++ advent2021/src/advent2021/Day05.scala
@@ -11,30 +11,50 @@ object Day05 extends zio.App {
     try source.getLines().toList finally source.close()
   }
 
-  def avoidDangerAreas(input: List[String]): Long = {
+  def avoidDangerAreasPart2(input: List[String], vertical: Boolean): Long = {
     val parsedInput: Seq[((Int, Int), (Int, Int))] = input.map {
       case s"$a,$b -> $c,$d" => ((a.toInt, b.toInt), (c.toInt, d.toInt))
     }
 
-    val allPoints = parsedInput.flatMap {
-      case (aPoint, bPoint) =>
-        if (aPoint._1 == bPoint._1) {
-          val (startPoint, endPoint) = if (aPoint._2 <= bPoint._2) (aPoint, bPoint) else (bPoint, aPoint)
+    val sortedParsedInput = parsedInput.map {
+      case ((a, b), (x, y)) =>
+        if (a < x) {
+          ((a, b), (x, y))
+        } else if (a == x) {
+          if (b <= y) ((a, b), (x, y)) else ((x, y), (a, b))
+        } else {
+          ((x, y), (a, b))
+        }
+    }
+
+    val allPoints = sortedParsedInput.flatMap {
+      case (startPoint, endPoint) =>
+        if (startPoint._1 == endPoint._1) {
           val elements = endPoint._2 - startPoint._2 + 1
           List.tabulate(elements)(n => (startPoint._1, startPoint._2 + n))
-        } else if (aPoint._2 == bPoint._2) {
-          val (startPoint, endPoint) = if (aPoint._1 <= bPoint._1) (aPoint, bPoint) else (bPoint, aPoint)
+        } else if (startPoint._2 == endPoint._2) {
           val elements = endPoint._1 - startPoint._1 + 1
           List.tabulate(elements)(n => (startPoint._1 + n, startPoint._2))
+        } else if (startPoint._1 < endPoint._1 && startPoint._2 < endPoint._2) {
+          if (vertical) {
+            val elements = endPoint._1 - startPoint._1 + 1
+            List.tabulate(elements)(n => (startPoint._1 + n, startPoint._2 + n))
+          } else {
+            List.empty
+          }
         } else {
-          List.empty
+          if (vertical) {
+            val elements = endPoint._1 - startPoint._1 + 1
+            List.tabulate(elements)(n => (startPoint._1 + n, startPoint._2 - n))
+          } else {
+            List.empty
+          }
         }
     }
 
     allPoints.groupMapReduce(identity)(_ => 1)(_ + _).toList.count(e => e._2 > 1)
   }
 
-  // winner = true for part 1, winner = false for part 2
   def run(args: List[String]) =
-    readInput.map(avoidDangerAreas).flatMap(r => putStrLn(r.toString)).exitCode
+    readInput.map(i => avoidDangerAreasPart2(i, true)).flatMap(r => putStrLn(r.toString)).exitCode
 }
