36fa096d4102b0e983ceef2e649f6599c229c2ee
Mario Huizar <mario.huizar@gmail.com>
2021-12-07 17:12:50 +0100

    Day 7 Part 2

--- advent2021/src/advent2021/Day07.scala
+++ advent2021/src/advent2021/Day07.scala
@@ -2,20 +2,38 @@ package advent2021
 
 import zio.console._
 
+import scala.annotation.tailrec
 import scala.io.Source
 
 object Day07 extends zio.App {
   private val readInput = zio.blocking.effectBlocking {
     val source = Source.fromFile("advent2021/src/resources/Day07Input.txt")
-    try source.getLines().toList finally source.close()
+    try source.getLines().toList.flatMap(_.split(",").toList).map(_.toInt) finally source.close()
   }
 
-  def fuelCalculator(input: List[String]): Long = {
-    val crabPositions = input.flatMap(_.split(",").toList).map(_.toInt)
-    val fuelNeededForAllPositions = for (p <- crabPositions) yield crabPositions.map { e => Math.abs(e - p) }.sum
+  def fuelCalculator(crabPositions: List[Int]): Long = {
+    val fuelNeededForAllPositions = for (p <- 0 to crabPositions.size) yield crabPositions.map { e => Math.abs(e - p) }.sum
+    fuelNeededForAllPositions.min
+  }
+
+  def fuelCalculatorPart2(crabPositions: List[Int]): Long = {
+    @tailrec def stepCalculator(steps: Long, acc: Long): Long = {
+      if (steps == 0) {
+        acc
+      } else {
+        stepCalculator(steps - 1, acc + steps)
+      }
+    }
+
+    val fuelNeededForAllPositions = for (p <- 0 to crabPositions.size)
+      yield crabPositions.map { e => {
+        stepCalculator(Math.abs(e - p), 0)
+      }
+      }.sum
+
     fuelNeededForAllPositions.min
   }
 
   def run(args: List[String]) =
-    readInput.map(fuelCalculator).flatMap(r => putStrLn(r.toString)).exitCode
+    readInput.map(fuelCalculatorPart2).flatMap(r => putStrLn(r.toString)).exitCode
 }
