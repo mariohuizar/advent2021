88f35efd7881a5c4ccf7bc11ac032188f30e23c7
Mario Huizar <mario.rodriguez@jambit.com>
2021-12-12 16:44:12 +0100

    Day 9 Part 1

--- advent2021/src/advent2021/Day09.scala
+++ advent2021/src/advent2021/Day09.scala
@@ -10,29 +10,31 @@ object Day09 extends zio.App {
     try source.getLines().toList finally source.close()
   }
 
-  final case class Node(value: Int, up: Option[Int], down: Option[Int], left: Option[Int], right: Option[Int])
+  final case class Node(height: Int, up: Option[Int], down: Option[Int], left: Option[Int], right: Option[Int])
 
   def findLowPoints(input: List[String]): Long = {
     val inputAsInts = input.map(_.toList.map(_.asDigit))
 
+    // transform three adjacent line of ints to a single node list
     def getNodeLine(line: List[Option[Int]], upLine: List[Option[Int]], downLine: List[Option[Int]]): List[Node] = {
       val leftRightNeighbors = line.sliding(3).map {
-        case l :: Some(e) :: r :: Nil => Node(value = e, up = None, down = None, left = l, right = r)
+        case l :: Some(e) :: r :: Nil => Node(height = e, up = None, down = None, left = l, right = r)
       }.toList
       upLine.zip(downLine).tail.zip(leftRightNeighbors).map { e => e._2.copy(up = e._1._1, down = e._1._2) }
     }
 
-    val inputAsSomeInts = inputAsInts.map(_.map(Some(_))).map{l => (None :: l) :+ None}
-
-    val intermediateNodes = inputAsSomeInts.sliding(3, 1).toList.map{ t => getNodeLine(t(1), t.head, t(2))}
+    // add None elements at start and end to do a sliding(3, 1) through all data
+    val inputAsSomeInts = inputAsInts.map(_.map(Some(_))).map { l => (None :: l) :+ None }
     val emptyIntList = List.fill(inputAsSomeInts.head.size)(None)
-    val firstLineNodes = getNodeLine(inputAsSomeInts.head, emptyIntList, inputAsSomeInts(1))
-    val lastLineNodes = getNodeLine(inputAsSomeInts.last, inputAsSomeInts((inputAsSomeInts.length - 2)), emptyIntList)
+    val inputWithPadding = (emptyIntList :: inputAsSomeInts) :+ emptyIntList
+    val nodes = inputWithPadding.sliding(3, 1).toList.map { t => getNodeLine(t(1), t.head, t(2)) }
 
-    val allNodes = (firstLineNodes :: intermediateNodes) :+ lastLineNodes
+    val lowPoints = nodes.flatten.filter( { n: Node =>
+        (n.height < n.up.getOrElse(10)) && (n.height < n.down.getOrElse(10)) &&
+          (n.height < n.left.getOrElse(10)) && (n.height < n.right.getOrElse(10))
+    } )
 
-    //val lowPoints = allNodes.filterNot{ n: Node => n.value < n.up.getOrElse(10) }
-    0
+    lowPoints.map{ e => 1 + e.height}.sum
   }
 
   def run(args: List[String]) =
