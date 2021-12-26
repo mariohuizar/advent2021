1d86e4e50042c877a40e84ddc4e83c22cd33fbfc
Mario Huizar <mario.rodriguez@jambit.com>
2021-12-26 23:22:05 +0100

    Day 10 Part 2

--- advent2021/src/advent2021/Day10.scala
+++ advent2021/src/advent2021/Day10.scala
@@ -13,26 +13,44 @@ object Day10 extends zio.App {
     try source.getLines().toList finally source.close()
   }
 
-  def syntaxErrorScore(input: List[String]): Int = {
+  @tailrec def findFirstIllegalCharacter(line: String, acc: List[Char], currentPos: Int): Option[Char] = {
     val pairs = Map('{' -> '}', '(' -> ')', '[' -> ']', '<' -> '>')
-    val scores = Map('}' -> 1197, ')' -> 3, ']' -> 57, '>' -> 25137)
-
-    @tailrec def findFirstIllegalCharacter(line: String, acc: List[Char], currentPos: Int): Option[Char] = {
-      if (line.length == currentPos)
-        None
-      else {
-        val currentChar = line(currentPos)
-        if (currentChar == '[' || currentChar == '(' || currentChar == '{' || currentChar == '<') {
-          findFirstIllegalCharacter(line, currentChar :: acc, currentPos + 1)
-        } else {
-          if (pairs.getOrElse(acc.head, 'e').equals(currentChar))
-            findFirstIllegalCharacter(line, acc.tail, currentPos + 1)
-          else {
-            Some(currentChar)
-          }
+    if (line.length == currentPos)
+      None
+    else {
+      val currentChar = line(currentPos)
+      if (currentChar == '[' || currentChar == '(' || currentChar == '{' || currentChar == '<') {
+        findFirstIllegalCharacter(line, currentChar :: acc, currentPos + 1)
+      } else {
+        if (pairs.getOrElse(acc.head, 'e').equals(currentChar))
+          findFirstIllegalCharacter(line, acc.tail, currentPos + 1)
+        else {
+          Some(currentChar)
         }
       }
     }
+  }
+
+  @tailrec def getClosingCharacters(line: String, acc: List[Char], currentPos: Int): List[Char] = {
+    val pairs = Map('{' -> '}', '(' -> ')', '[' -> ']', '<' -> '>')
+    if (line.length == currentPos)
+      acc.map { e => pairs.getOrElse(e, 'e') }
+    else {
+      val currentChar = line(currentPos)
+      if (currentChar == '[' || currentChar == '(' || currentChar == '{' || currentChar == '<') {
+        getClosingCharacters(line, currentChar :: acc, currentPos + 1)
+      } else {
+        if (pairs.getOrElse(acc.head, 'e').equals(currentChar))
+          getClosingCharacters(line, acc.tail, currentPos + 1)
+        else {
+          acc.map { e => pairs.getOrElse(e, 'e') }
+        }
+      }
+    }
+  }
+
+  def syntaxErrorScore(input: List[String]): Long = {
+    val scores = Map('}' -> 1197, ')' -> 3, ']' -> 57, '>' -> 25137)
 
     input.flatMap {
       findFirstIllegalCharacter(_, List.empty, 0)
@@ -41,6 +59,33 @@ object Day10 extends zio.App {
     }.sum
   }
 
+  def incompleteLinesScore(input: List[String]): Long = {
+    val points = Map('}' -> 3L, ')' -> 1L, ']' -> 2L, '>' -> 4L)
+    // discard syntax error lines
+    val incompleteLines = input
+      .map { e => (findFirstIllegalCharacter(e, List.empty, 0), e) }
+      .flatMap {
+        case (None, e) => Some(e)
+        case _ => None
+      }
+
+    val closingCharacters = incompleteLines.map {
+      getClosingCharacters(_, List.empty, 0)
+    }
+    val scores = closingCharacters.map {
+      _.map {
+        points.getOrElse(_, 0L)
+      }
+    }.map { l =>
+      l.foldLeft(0L) { (acc, e) =>
+        acc * 5 + e
+      }
+    }
+
+    val scoresSorted = scores.sorted
+    scoresSorted((scores.length - 1) / 2)
+  }
+
   def run(args: List[String]) =
-    readInput.map(syntaxErrorScore).flatMap(r => putStrLn(r.toString)).exitCode
+    readInput.map(incompleteLinesScore).flatMap(r => putStrLn(r.toString)).exitCode
 }
